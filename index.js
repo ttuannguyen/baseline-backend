b6fc11767a75022b2ef9af6c97d9a88cfeb9e1bc
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-31 18:54:52 -0700

    build the post request with the user's input and hit the edamam api from the server

--- index.js
+++ index.js
@@ -5,18 +5,35 @@ const axios = require('axios');
 // Create express server
 const app = express();
 app.use(cors());
-app.use(axios);
+app.use(express.json()); 
+// app.use(axios);
 
 const EDAMAM_API_ID = '805a07f7';
 const EDAMAM_API_KEY = '48484be0535a5017a5e879330aa66712';
 
 app.get('/api', (req, res) => {
-    res.send('Hello from server!');
+  res.send('Hello from server!');
 })
 
-app.post('/api/search', (req, res) => {
-    res.json({'message': 'form submitted'})
-})
+app.post('/api/search', async (req, res) => {
+  const { keyword } = req.body;
+  console.log('Keyword:', keyword);
+
+  try {
+    const response = await axios.get('https://api.edamam.com/search', {
+      params: {
+        q: keyword,
+        app_id: EDAMAM_API_ID,
+        app_key: EDAMAM_API_KEY,
+      },
+    });
+
+    console.log(response.data.hits);
+    res.json('tbd');
+  } catch (error) {
+    console.log(error)
+  }
+});
 
 
 // Start listening on env.port
